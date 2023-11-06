7938e09287a155bd6c537622363999f762949ae9
Brandon Nelson <nelsonb3@oregonstate.edu>
2023-11-05 16:59:47 -0800

    Exercise 5 complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Brandon Nelson
 # CS361_Exercise5
+
+
+Tbis is a refactor of code provided that is to create users and a host to be saved as part of a discussion
--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 # Exercise 5
 
 class DiscussionWorkflow
-
-    def initialize(discussion, host)
+    attr_accessor :discussion
+    def initialize(discussion)
       @discussion = discussion
-      @host = host
     end
   
     # Expects @participants array to be filled with User objects
@@ -24,40 +23,55 @@ class DiscussionWorkflow
     # ...
   
   end
-  
 
+class User
+  attr_accessor :email, :password
+
+  def initialize(email)
+    @email = email
+    @password = password
+  end
+
+  def create_from_email(email, password)
+    new(email.downcase)
+  end
+
+end
 
 
 class Participant
-
-    def initialize(participants, participants_email_string)
-       @participants = []
-       @participants_email_string = participants_email_string
-    end
-
-
-    def generate_participant_users_from_email_string
-        return if @participants_email_string.blank?
-            @participants_email_string.split.uniq.map do |email_address|
-            User.create(email: email_address.downcase, password: Device.friendly_token)
-        end
-    end
+    attr_accessor :participants, :participants_email_string
+  
+  def initialize(participants, participants_email_string)
+      @participants = []
+      @participants_email_string = participants_email_string
+  end
 
 
-  class Discussion
+  def generate_participant_users_from_email_string(email_string)
+      return if @participants_email_string.blank?
+          @participants_email_string.split.uniq.map do |email_address|
+          User.create_from_email(email: email_address.downcase, password: Device.friendly_token)
+      end
+  end
 
-    def initialize(title, host, participants_list)
-        @title = title
-        @host = host
-        @participants_list = participants
-    end
+
+class Discussion
+  attr_accessor :title, :host, :participants
+
+  def initialize(title, host, participants_list)
+      @title = title
+      @host = host
+      @participants = participants
+  end
+end
     
 
 
-  discussion = Discussion.new(title: "fake", ...)
+  participants = Participant.new(50,"fake1@example.com\nfake2@example.com\nfake3@example.com" )
   host = User.find(42)
-  participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
-  
-  workflow = DiscussionWorkflow.new(discussion, host, participants)ikr6qat62
+  discussion = Discussion.new(title: "fake", host, participants)
+
+  workflow = DiscussionWorkflow.new(discussion)
   workflow.generate_participant_users_from_email_string
-  workflow.run
+  workflow.launch_discussion_workflow
